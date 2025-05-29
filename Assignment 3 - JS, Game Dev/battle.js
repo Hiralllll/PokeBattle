bb6518cc387ef81979b1b12798b6e51c64c58c9a
Hiral Bapna <112919849+Hiralllll@users.noreply.github.com>
2025-05-29 17:42:52 +0530

    add turn logic in battle.js

--- Assignment 3 - JS, Game Dev/battle.js
+++ Assignment 3 - JS, Game Dev/battle.js
@@ -14,6 +14,8 @@ FetchPokeData2(poke_id_2);
 HP(hpval1);
 HP(hpval2);
 
+let turn=0;
+
 function HP(hpval) {
     if (hpval.value > (0.6 * hpval.max)) {
         hpval.style.accentColor = "#36a854";
@@ -24,7 +26,6 @@ function HP(hpval) {
     }
 }
 
-
 async function FetchPokeData1(id) {
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     let data = await response.json();
@@ -34,16 +35,16 @@ async function FetchPokeData1(id) {
     document.getElementById("name1").innerText = poke1;
 
     document.getElementById("move1").innerText = data.moves[0].move.name;
-    document.getElementById("move1").addEventListener("click", () => Battle(poke_id_1, poke_id_2, data.moves[0].move.name));
+    document.getElementById("move1").addEventListener("click", () => decide_turn_1(0, turn, data));
 
     document.getElementById("move2").innerText = data.moves[1].move.name;
-    document.getElementById("move2").addEventListener("click", () => Battle(poke_id_1, poke_id_2, data.moves[1].move.name));
+    document.getElementById("move2").addEventListener("click", () => decide_turn_1(1, turn, data));
 
     document.getElementById("move3").innerText = data.moves[2].move.name;
-    document.getElementById("move3").addEventListener("click", () => Battle(poke_id_1, poke_id_2, data.moves[2].move.name));
+    document.getElementById("move3").addEventListener("click", () => decide_turn_1(2, turn, data));
 
     document.getElementById("move4").innerText = data.moves[3].move.name;
-    document.getElementById("move4").addEventListener("click", () => Battle(poke_id_1, poke_id_2, data.moves[3].move.name));
+    document.getElementById("move4").addEventListener("click", () => decide_turn_1(3, turn, data));
 }
 
 async function FetchPokeData2(id) {
@@ -55,17 +56,36 @@ async function FetchPokeData2(id) {
     document.getElementById("name2").innerText = poke2;
 
     document.getElementById("move5").innerText = data.moves[0].move.name;
-    document.getElementById("move5").addEventListener("click", () => Battle(poke_id_2, poke_id_1, data.moves[0].move.name));
+    document.getElementById("move5").addEventListener("click", () => decide_turn_2(0, turn, data));
 
     document.getElementById("move6").innerText = data.moves[1].move.name;
-    document.getElementById("move6").addEventListener("click", () => Battle(poke_id_2, poke_id_1, data.moves[1].move.name));
+    document.getElementById("move6").addEventListener("click", () => decide_turn_2(1, turn, data));
 
     document.getElementById("move7").innerText = data.moves[2].move.name;
-    document.getElementById("move7").addEventListener("click", () => Battle(poke_id_2, poke_id_1, data.moves[2].move.name));
+    document.getElementById("move7").addEventListener("click", () => decide_turn_2(2, turn, data));
 
     document.getElementById("move8").innerText = data.moves[3].move.name;
-    document.getElementById("move8").addEventListener("click", () => Battle(poke_id_2, poke_id_1, data.moves[3].move.name));
+    document.getElementById("move8").addEventListener("click", () => decide_turn_2(3, turn, data));
 
+}
+
+function decide_turn_1(i, myturn, data){
+    if (myturn===0){
+        Battle(poke_id_1, poke_id_2, data.moves[i].move.name);
+        turn=1;
+        console.log(turn);
+    } else {
+        document.getElementById("log").innerText = "Not your turn";
+    }
+}
+
+function decide_turn_2(i, myturn, data){
+    if (myturn===1){
+        Battle(poke_id_2, poke_id_1, data.moves[i].move.name);
+        turn=0;
+    } else {
+        document.getElementById("log").innerText = "Not your turn";
+    }
 }
 
 async function DamageRelations(type, DefenderType) {
@@ -87,7 +107,6 @@ async function DamageRelations(type, DefenderType) {
 }
 
 async function Battle(attacker_id, defender_id, move) {
-
     let poke_attacker = await fetch(`https://pokeapi.co/api/v2/pokemon/${attacker_id}`);
     let poke_attack_stats = await poke_attacker.json();
 
@@ -121,7 +140,7 @@ async function Battle(attacker_id, defender_id, move) {
         HP(hpval2);
     };
 
-    document.getElementById("log").innerText = `${AttackerName} used ${move}`
+    document.getElementById("log").innerText = `${AttackerName} used ${move}.`
     console.log(`damage inflicted =${damage}`);
-}
+} 
 
